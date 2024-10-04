18327d6befa9fd6fdbbde1d33299d1d66f52df3b
khoadoanduy <khoadoanduy05@gmail.com>
2024-10-03 23:03:36 -0500

    Succeeded connecting to supabase, tested inserting and printing data

--- app/recipe/page.js
+++
@@ -1,9 +0,0 @@
-export default function RecipeCreatorPage() {
-    return (
-      <div>
-        <h1>Recipe Creator</h1>
-        {/* Implement recipe creator logic */}
-      </div>
-    );
-  }
-  
--- app/recommendation/page.js
+++
@@ -1,9 +0,0 @@
-export default function RecommendationPage() {
-    return (
-      <div>
-        <h1>Further Wine Recommendation</h1>
-        {/* Implement further logic for wine recommendations */}
-      </div>
-    );
-  }
-  
